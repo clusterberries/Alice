ca61f3ad445bdec25ffb835db9f77d53cddb3722
Alexandra Govorkova <al.govorkova@gmail.com>
2016-11-12 21:08:24 +0300

    Add game #2

---
+++ Assets/Scripts/game2Ctrl.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class game2Ctrl : MonoBehaviour {
+	public GameObject ring;
+	public GameObject nextBtn;
+	public Book book;
+
+	public GameObject bar;
+	private float barLenght;
+	private RectTransform barTr;
+
+	private bool gameInProgress = true;
+
+	private static int score = 0;
+	private float ringsCount = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+		barTr = bar.GetComponent<RectTransform> ();
+		barLenght = barTr.sizeDelta.x;
+		barTr.sizeDelta = new Vector2 (0.0f, barTr.sizeDelta.y);
+
+		nextBtn.SetActive(false);
+
+		StartCoroutine (SpawnRings());
+	}
+
+	IEnumerator SpawnRings () {
+		while (gameInProgress) {
+			Vector3 spawnPosition = new Vector3(0, 0, -3);
+			Quaternion spawnRotation = Quaternion.identity;
+			GameObject newRing = Instantiate(ring, spawnPosition, spawnRotation) as GameObject;
+			newRing.transform.parent = gameObject.transform;
+
+			yield return new WaitForSeconds(2);
+		}
+	}
+
+	public void SetScore () {
+		score++;
+		SetBar ((float)score / ringsCount);
+		if (score == (int)ringsCount) {
+			gameInProgress = false;
+			nextBtn.SetActive(true);
+			book.ShowWon ();
+		}
+	}
+
+	public void SetBar (float pr) {
+		barTr.sizeDelta = new Vector2 (Mathf.Min(barLenght * pr, barLenght), barTr.sizeDelta.y);
+	}
+}
---
+++ Assets/Scripts/ringCtrl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ringCtrl : MonoBehaviour {
+	private SpriteRenderer sr;
+	private game2Ctrl game;
+	private Color[] colors = new Color[4];
+
+	// Use this for initialization
+	void Start () {
+		colors [0] = Color.yellow;
+		colors [1] = Color.blue;
+		colors [2] = Color.green;
+		colors [3] = Color.magenta;
+
+		game = GameObject.Find ("Page4").GetComponent<game2Ctrl>();
+
+		sr = GetComponent<SpriteRenderer> ();
+		sr.color = colors[Random.Range(0, 4)];
+	}
+
+	void OnMouseDown () {
+		game.SetScore ();
+		Destroy (gameObject);
+	}
+}
