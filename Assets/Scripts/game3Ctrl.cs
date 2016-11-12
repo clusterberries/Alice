f31c93a29b06ba33337722f6942c76bc60a3943d
Alexandra Govorkova <al.govorkova@gmail.com>
2016-11-12 22:45:47 +0300

    Create game #3

---
+++ Assets/Scripts/game3Ctrl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class game3Ctrl : MonoBehaviour {
+	public GameObject nextBtn;
+	public Book book;
+
+	static int score = 0;
+	private int rosesCount = 14;
+
+	// Use this for initialization
+	void Start () {
+		nextBtn.SetActive(false);
+	}
+
+	public void AddScore () {
+		score++;
+		if (score == rosesCount) {
+			nextBtn.SetActive(true);
+			book.ShowWon ();
+		}
+	}
+}
---
+++ Assets/Scripts/roseCtrl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class roseCtrl : MonoBehaviour {
+	private Animator anim;
+	private game3Ctrl game;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+		game = GameObject.Find ("Page6").GetComponent<game3Ctrl>();
+	}
+
+	void OnMouseDown() {
+		anim.Play("rose");
+		game.AddScore ();
+	}
+}
