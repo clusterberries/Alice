d7a62c449d167a99b87d7a12441fd62b28bcee57
Alexandra Govorkova <al.govorkova@gmail.com>
2016-11-12 21:55:30 +0300

    Add page #5

---
+++ Assets/Scripts/catFadeCtrl.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class catFadeCtrl : MonoBehaviour {
+	private Animator anim;
+	private bool isShown = false;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	void OnMouseDown() {
+		isShown = !isShown;
+		anim.Play(isShown ? "catFade" : "catShow");
+	}
+}
